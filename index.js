daac82a611b8ce2785ef1f781d9e8010c3c67e46
Ingvar Stepanyan <me@rreverser.com>
2013-12-04 14:02:27 +0200

    Added module.exports && exports references.

--- index.js
+++ index.js
@@ -21,59 +21,72 @@ if (!program.output) {
 	program.output = program.input.replace(jsExtRegExp, '.out.js');
 }
 
-var Replacer = Visitor.extend({
-	modulesId: b.identifier('$MODULES'),
+var modulesId = b.identifier('$MODULES'),
+	moduleId = b.identifier('module'),
+	exportsId = b.identifier('exports'),
+	moduleExportsMember = b.memberExpression(moduleId, exportsId, false),
+	modulePreamble = b.variableDeclaration('var', [
+		b.variableDeclarator(exportsId, b.objectExpression([])),
+		b.variableDeclarator(moduleId, b.objectExpression([
+			b.property('init', exportsId, exportsId)
+		]))
+	]),
+	moduleReturn = b.returnStatement(moduleExportsMember),
 
-	init: function (modulePath, parent) {
-		this.ids = parent ? parent.ids : {};
-		this.modulesArray = parent ? parent.modulesArray : [];
-		this.hasParent = !!parent;
-		this.path = '';
-		this.path = this.toAbsolute(modulePath);
-	},
+	Replacer = Visitor.extend({
+		init: function (modulePath, parent) {
+			this.ids = parent ? parent.ids : {};
+			this.modulesArray = parent ? parent.modulesArray : [];
+			this.hasParent = !!parent;
+			this.path = '';
+			this.path = this.toAbsolute(modulePath);
+		},
 
-	getId: function (name) {
-		return name in this.ids ? this.ids[name] : (this.ids[name] = this.modulesArray.push());
-	},
+		getId: function (name) {
+			return name in this.ids ? this.ids[name] : (this.ids[name] = this.modulesArray.push());
+		},
 
-	toAbsolute: function (modulePath) {
-		return url.resolve(this.path, modulePath).replace(jsExtRegExp, '.js');
-	},
+		toAbsolute: function (modulePath) {
+			return url.resolve(this.path, modulePath).replace(jsExtRegExp, '.js');
+		},
 
-	visitCallExpression: function (node) {
-		if (n.Identifier.check(node.callee) && node.callee.name === 'require' && n.Literal.check(node.arguments[0])) {
-			var path = this.toAbsolute(node.arguments[0].value);
+		visitCallExpression: function (node) {
+			if (n.Identifier.check(node.callee) && node.callee.name === 'require' && n.Literal.check(node.arguments[0])) {
+				var path = this.toAbsolute(node.arguments[0].value);
 
-			new Replacer(path, this).visit(recast.parse(fs.readFileSync(path, {encoding: 'utf-8'})));
+				new Replacer(path, this).visit(recast.parse(fs.readFileSync(path, {encoding: 'utf-8'})));
 
-			return b.callExpression(
-				b.memberExpression(
-					this.modulesId,
-					b.literal(this.getId(path)),
-					true
-				),
-				node.arguments.slice(1)
-			);
+				return b.callExpression(
+					b.memberExpression(
+						modulesId,
+						b.literal(this.getId(path)),
+						true
+					),
+					node.arguments.slice(1)
+				);
+			}
+
+			this.genericVisit(node);
+		},
+
+		visitProgram: function (node) {
+			node.body.unshift(modulePreamble);
+			node.body.push(moduleReturn);
+
+			this.modulesArray[this.getId(this.path)] = b.functionExpression(null, [], b.blockStatement(node.body));
+			this.visit(node.body);
+
+			if (!this.hasParent) {
+				return b.program([
+					b.variableDeclaration('var', [b.variableDeclarator(
+						modulesId,
+						b.arrayExpression(this.modulesArray)
+					)]),
+					b.expressionStatement(b.callExpression(b.memberExpression(modulesId, b.literal(0), true), []))
+				]);
+			}
 		}
-
-		this.genericVisit(node);
-	},
-
-	visitProgram: function (node) {
-		this.modulesArray[this.getId(this.path)] = b.functionExpression(null, [], b.blockStatement(node.body));
-		this.visit(node.body);
-
-		if (!this.hasParent) {
-			return b.program([b.variableDeclaration(
-				'var',
-				[b.variableDeclarator(
-					this.modulesId,
-					b.arrayExpression(this.modulesArray)
-				)]
-			)]);
-		}
-	}
-});
+	});
 
 console.log(program.input);
 var ast = recast.parse(fs.readFileSync(program.input, {encoding: 'utf-8'}));
--- sample/a.out.js
+++ sample/a.out.js
@@ -1,7 +1,27 @@
 var $MODULES = [function() {
+        var exports = {},
+            module = {
+                exports: exports
+            }
+
         module.exports = $MODULES[1]() * 2;
+        return module.exports;
     }, function() {
+        var exports = {},
+            module = {
+                exports: exports
+            }
+
         module.exports = $MODULES[2]().value * 7;
+        return module.exports;
     }, function() {
+        var exports = {},
+            module = {
+                exports: exports
+            }
+
         exports.value = 3;
+        return module.exports;
     }];
+
+$MODULES[0]();
